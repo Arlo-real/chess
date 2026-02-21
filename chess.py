221d5e6937fc642049410d3287e5da2df519032b
Arlo <arlo-real@protonmail.com>
2026-02-21 21:22:51 +0100

    flipped

--- chess.py
+++ chess.py
@@ -6,13 +6,20 @@ from math import floor
 # self.previouspiece is the index of the piece that is selected, not the piece itself
 # it is used to store what piece is selected (or not) between button clicks
 
+# If you are confused by the self.blackpieces[0] etc, just repalce them with the hardcoded values
+# This was done to allow to make the pieces rotate but makes the code much less readable
+
 class chess:
     def __init__(self):
+
+        self.whitepieces = ["♙", "♖", "♘", "♗", "♕", "♔"]
+        self.blackpieces = ["♟", "♜", "♞", "♝", "♛", "♚"]
+
+
         self.enpassantenabled = True
         self.kastlingenabled = True
         self.turnboardenabled = True
-        self.blackpieces = ["♟", "♜", "♞", "♝", "♛", "♚"]
-        self.whitepieces = ["♙", "♖", "♘", "♗", "♕", "♔"]
+        self.blackpieces = ["🨩", "🨦", "🨨", "🨧", "🨥", "🨤"]
 
         if self.kastlingenabled:
             self.whitecastlingkingside = True
@@ -35,12 +42,12 @@ class chess:
         self.playerinfolabel=tk.Label(self.frame, font=("Arial", 10))
         self.playerinfolabel.grid(column=1, columnspan=8)
         self.list_of_cases= []
-        self.list_of_pieces= ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"]
-        self.list_of_pieces= [self.blackpieces[1], "♞", "♝", "♛", "♚", "♝", "♞", "♜"]
+        self.list_of_pieces= [self.blackpieces[1], self.blackpieces[2], self.blackpieces[3], self.blackpieces[4], self.blackpieces[5], self.blackpieces[3], self.blackpieces[2], self.blackpieces[1]]
+        self.list_of_pieces= [self.blackpieces[1], self.blackpieces[2], self.blackpieces[3], self.blackpieces[4], self.blackpieces[5], self.blackpieces[3], self.blackpieces[2], self.blackpieces[1]]
         for i in range (8): self.list_of_pieces.append(self.blackpieces[0])
         for i in range (32): self.list_of_pieces.append("")
         for i in range (8): self.list_of_pieces.append(self.whitepieces[0])
-        self.list_of_pieces.extend(["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"])
+        self.list_of_pieces.extend([self.whitepieces[1], self.whitepieces[2], self.whitepieces[3], self.whitepieces[4], self.whitepieces[5], self.whitepieces[3], self.whitepieces[2], self.whitepieces[1]])
         self.list_of_colors = []
         for i in range(16): self.list_of_colors.append("black")
         for i in range(32): self.list_of_colors.append("green") #if I forget, it will stand out
@@ -132,19 +139,19 @@ class chess:
                     self.updatecolor(i, "green")
         if self.kastlingenabled:
             # Update castling rights if king or rooks move / are captured
-            if self.getpiece(origin) == "♔":
+            if self.getpiece(origin) == self.whitepieces[5]:
                 self.whitecastlingkingside = False
                 self.whitecastlingqueenside = False
-            elif self.getpiece(origin) == "♚":
+            elif self.getpiece(origin) == self.blackpieces[5]:
                 self.blackcastlingkingside = False
                 self.blackcastlingqueenside = False
-            if self.getpiece(0) != "♜":
+            if self.getpiece(0) != self.blackpieces[1]:
                 self.blackcastlingqueenside = False
-            if self.getpiece(7) != "♜":
+            if self.getpiece(7) != self.blackpieces[1]:
                 self.blackcastlingkingside = False
-            if self.getpiece(56) != "♖":
+            if self.getpiece(56) != self.whitepieces[1]:
                 self.whitecastlingqueenside = False
-            if self.getpiece(63) != "♖":
+            if self.getpiece(63) != self.whitepieces[1]:
                 self.whitecastlingkingside = False
 
         # Move the piece
@@ -152,9 +159,9 @@ class chess:
         self.list_of_pieces[origin] = ""
 
         # Handle rook movement for castling
-        if self.kastlingenabled and self.getpiece(destination) in ["♔", "♚"] and abs(self.column(destination) - self.column(origin)) == 2 and self.row(destination) == self.row(origin):
+        if self.kastlingenabled and self.getpiece(destination) in [self.whitepieces[5], self.blackpieces[5]] and abs(self.column(destination) - self.column(origin)) == 2 and self.row(destination) == self.row(origin):
             # White castling
-            if self.getpiece(destination) == "♔":
+            if self.getpiece(destination) == self.whitepieces[5]:
                 # Kingside: e1 (60) -> g1 (62), rook h1 (63) -> f1 (61)
                 if destination == 62:
                     self.list_of_pieces[61] = self.list_of_pieces[63]
@@ -170,7 +177,7 @@ class chess:
                 self.whitecastlingkingside = False
                 self.whitecastlingqueenside = False
             # Black castling
-            elif self.getpiece(destination) == "♚":
+            elif self.getpiece(destination) == self.blackpieces[5]:
                 # Kingside: e8 (4) -> g8 (6), rook h8 (7) -> f8 (5)
                 if destination == 6:
                     self.list_of_pieces[5] = self.list_of_pieces[7]
@@ -185,13 +192,13 @@ class chess:
                     self.updatecolor(0, "green")
                 self.blackcastlingkingside = False
                 self.blackcastlingqueenside = False
-        if self.enpassantenabled and self.getpiece(destination) in ["♙", "♟"] and abs(self.row(destination)-self.row(origin))==2:
+        if self.enpassantenabled and self.getpiece(destination) in [self.whitepieces[0], self.blackpieces[0]] and abs(self.row(destination)-self.row(origin))==2:
             self.list_of_pieces[self.getindex(self.column(destination), (self.row(destination)+self.row(origin))//2)] = "ep"
             self.updatecolor(self.getindex(self.column(destination), (self.row(destination)+self.row(origin))//2), "green")
         if not simulation:
-            if self.row(destination)==8 and self.getpiece(destination)=="♟":
+            if self.row(destination)==8 and self.getpiece(destination)==self.blackpieces[0]:
                 self.promote_pawn(destination)
-            if self.row(destination)==1 and self.getpiece(destination)=="♙":
+            if self.row(destination)==1 and self.getpiece(destination)==self.whitepieces[0]:
                 self.promote_pawn(destination)
     
     def promote_pawn(self, index):
@@ -199,10 +206,10 @@ class chess:
         aux.title("Tic-Tac-Toe")
         frame=tk.Frame(aux, relief="ridge", bd=5)
         frame.pack()
-        if self.getpiece(index) == "♙":
-            list_of_possibilities= ["♕","♖","♗","♘"]
+        if self.getpiece(index) == self.whitepieces[0]:
+            list_of_possibilities= [self.whitepieces[4],self.whitepieces[1],self.whitepieces[3],self.whitepieces[2]]
         else:
-            list_of_possibilities= ["♛","♜","♝","♞"]
+            list_of_possibilities= [self.blackpieces[4],self.blackpieces[1],self.blackpieces[3],self.blackpieces[2]]
         for i in range(4):
             btn = tk.Button(frame, text=list_of_possibilities[i], font=("Arial", 20), command=lambda i=i: self.promote_and_close(index, list_of_possibilities[i], aux))
             btn.grid(column=i, row=0)
@@ -227,7 +234,7 @@ class chess:
         if not (origin is None or destination is None) and self.simulate_move(origin, destination):
             return False
         
-        if self.getpiece(origin) == "♙":
+        if self.getpiece(origin) == self.whitepieces[0]:
             if self.getpiece(destination) == "" and self.column(destination)==self.column(origin) and (
                 self.row(destination)==self.row(origin)-1 or (
                     self.row(origin)==7 and self.row(destination)==5 and self.getpiece(self.getindex(self.column(origin), 6))==""
@@ -239,7 +246,7 @@ class chess:
             if self.enpassantenabled and self.getpiece(destination) == "ep" and abs(self.column(destination)-self.column(origin))==1 and self.row(destination)==self.row(origin)-1:
                 return True
 
-        if self.getpiece(origin) == "♟":
+        if self.getpiece(origin) == self.blackpieces[0]:
             if self.getpiece(destination) == "" and self.column(destination)==self.column(origin) and (
                 self.row(destination)==self.row(origin)+1 or (
                     self.row(origin)==2 and self.row(destination)==4 and self.getpiece(self.getindex(self.column(origin), 3))==""
@@ -250,29 +257,29 @@ class chess:
                 return True
             if self.enpassantenabled and self.getpiece(destination) == "ep" and abs(self.column(destination)-self.column(origin))==1 and self.row(destination)==self.row(origin)+1:
                 return True
-        if self.getpiece(origin) == "♖" and self.column(destination)==self.column(origin) and self.row(destination)!=self.row(origin):
+        if self.getpiece(origin) == self.whitepieces[1] and self.column(destination)==self.column(origin) and self.row(destination)!=self.row(origin):
             for i in range(min(self.row(origin), self.row(destination))+1, max(self.row(origin), self.row(destination))):
                 if self.getpiece(self.getindex(self.column(origin), i))!="":
                     return False
             return True
-        if self.getpiece(origin) == "♖" and self.row(destination)==self.row(origin) and self.column(destination)!=self.column(origin):
+        if self.getpiece(origin) == self.whitepieces[1] and self.row(destination)==self.row(origin) and self.column(destination)!=self.column(origin):
             for i in range(min(self.column(origin), self.column(destination))+1, max(self.column(origin), self.column(destination))):
                 if self.getpiece(self.getindex(i, self.row(origin)))!="":
                     return False
             return True
-        if self.getpiece(origin) == "♜" and self.column(destination)==self.column(origin) and self.row(destination)!=self.row(origin):
+        if self.getpiece(origin) == self.blackpieces[1] and self.column(destination)==self.column(origin) and self.row(destination)!=self.row(origin):
             for i in range(min(self.row(origin), self.row(destination))+1, max(self.row(origin), self.row(destination))):
                 if self.getpiece(self.getindex(self.column(origin), i))!="":
                     return False
             return True
-        if self.getpiece(origin) == "♜" and self.row(destination)==self.row(origin) and self.column(destination)!=self.column(origin):
+        if self.getpiece(origin) == self.blackpieces[1] and self.row(destination)==self.row(origin) and self.column(destination)!=self.column(origin):
             for i in range(min(self.column(origin), self.column(destination))+1, max(self.column(origin), self.column(destination))):
                 if self.getpiece(self.getindex(i, self.row(origin)))!="":
                     return False
             return True
         
 
-        if self.getpiece(origin) == "♗" and abs(self.column(destination)-self.column(origin)) == abs(self.row(destination)-self.row(origin)):
+        if self.getpiece(origin) == self.whitepieces[3] and abs(self.column(destination)-self.column(origin)) == abs(self.row(destination)-self.row(origin)):
             steps=abs(self.column(destination)-self.column(origin))
             dowdirection=1 if self.row(destination)>self.row(origin) else -1
             columndirection=1 if self.column(destination)>self.column(origin) else -1
@@ -280,7 +287,7 @@ class chess:
                 if self.getpiece(self.getindex(self.column(origin)+i*columndirection, self.row(origin)+i*dowdirection))!="":
                     return False
             return True
-        if self.getpiece(origin) == "♝" and abs(self.column(destination)-self.column(origin)) == abs(self.row(destination)-self.row(origin)):
+        if self.getpiece(origin) == self.blackpieces[3] and abs(self.column(destination)-self.column(origin)) == abs(self.row(destination)-self.row(origin)):
             steps=abs(self.column(destination)-self.column(origin))
             dowdirection=1 if self.row(destination)>self.row(origin) else -1
             columndirection=1 if self.column(destination)>self.column(origin) else -1
@@ -289,7 +296,7 @@ class chess:
                     return False
             return True
         
-        if self.getpiece(origin) in ["♕", "♛"]:
+        if self.getpiece(origin) in [self.whitepieces[4], self.blackpieces[4]]:
             if self.column(destination)==self.column(origin) and self.row(destination)!=self.row(origin):
                 for i in range(min(self.row(origin), self.row(destination))+1, max(self.row(origin), self.row(destination))):
                     if self.getpiece(self.getindex(self.column(origin), i))!="":
@@ -309,11 +316,11 @@ class chess:
             else: return False
             return True        
         
-        if self.getpiece(origin) in ["♔", "♚"]:
+        if self.getpiece(origin) in [self.whitepieces[5], self.blackpieces[5]]:
             if abs(self.column(destination)-self.column(origin))<=1 and abs(self.row(destination)-self.row(origin))<=1:
                 return True
             elif self.kastlingenabled and abs(self.column(destination)-self.column(origin))==2 and self.row(destination)==self.row(origin):
-                if self.getpiece(origin) == "♔":
+                if self.getpiece(origin) == self.whitepieces[5]:
                     # White kingside: e1 -> g1 (destination 62), squares f1 (61) and g1 (62) must be empty
                     if destination == 62 and self.whitecastlingkingside and self.getpiece(61) == "" and self.getpiece(62) == "":
                         # Cannot castle out of, through, or into check
@@ -327,7 +334,7 @@ class chess:
                         if self.ischeck(self.player) or self.simulate_move(origin, middle):
                             return False
                         return True
-                if self.getpiece(origin) == "♚":
+                if self.getpiece(origin) == self.blackpieces[5]:
                     # Black kingside: e8 -> g8 (destination 6), squares f8 (5) and g8 (6) must be empty
                     if destination == 6 and self.blackcastlingkingside and self.getpiece(5) == "" and self.getpiece(6) == "":
                         middle = (origin + destination) // 2
@@ -343,7 +350,7 @@ class chess:
                 
                     
                 
-        if self.getpiece(origin) in ["♘", "♞"] and ((abs(self.column(destination)-self.column(origin))==2 and abs(self.row(destination)-self.row(origin))==1) or (abs(self.column(destination)-self.column(origin))==1 and abs(self.row(destination)-self.row(origin))==2)):
+        if self.getpiece(origin) in [self.whitepieces[2], self.blackpieces[2]] and ((abs(self.column(destination)-self.column(origin))==2 and abs(self.row(destination)-self.row(origin))==1) or (abs(self.column(destination)-self.column(origin))==1 and abs(self.row(destination)-self.row(origin))==2)):
             return True
         
         return False
@@ -390,13 +397,13 @@ class chess:
         col_d, row_d = self.column(destination), self.row(destination)
 
         # Pawns: only capture moves matter for check detection
-        if piece == "♙":  # white pawn
+        if piece == self.whitepieces[0]:  # white pawn
             return abs(col_d - col_o) == 1 and row_d == row_o - 1
-        if piece == "♟":  # black pawn
+        if piece == self.blackpieces[0]:  # black pawn
             return abs(col_d - col_o) == 1 and row_d == row_o + 1
 
         # Rooks
-        if piece in ["♖", "♜"]:
+        if piece in [self.whitepieces[1], self.blackpieces[1]]:
             if col_d == col_o and row_d != row_o:
                 for r in range(min(row_o, row_d) + 1, max(row_o, row_d)):
                     if self.getpiece(self.getindex(col_o, r)) != "":
@@ -409,7 +416,7 @@ class chess:
                 return True
 
         # Bishops
-        if piece in ["♗", "♝"]:
+        if piece in [self.whitepieces[3], self.blackpieces[3]]:
             if abs(col_d - col_o) == abs(row_d - row_o):
                 steps = abs(col_d - col_o)
                 row_dir = 1 if row_d > row_o else -1
@@ -420,7 +427,7 @@ class chess:
                 return True
 
         # Queens
-        if piece in ["♕", "♛"]:
+        if piece in [self.whitepieces[4], self.blackpieces[4]]:
             if col_d == col_o and row_d != row_o:
                 for r in range(min(row_o, row_d) + 1, max(row_o, row_d)):
                     if self.getpiece(self.getindex(col_o, r)) != "":
@@ -441,11 +448,11 @@ class chess:
                 return True
 
         # Kings
-        if piece in ["♔", "♚"]:
+        if piece in [self.whitepieces[5], self.blackpieces[5]]:
             return abs(col_d - col_o) <= 1 and abs(row_d - row_o) <= 1
 
         # Knights
-        if piece in ["♘", "♞"]:
+        if piece in [self.whitepieces[2], self.blackpieces[2]]:
             return (abs(col_d - col_o) == 2 and abs(row_d - row_o) == 1) or \
                    (abs(col_d - col_o) == 1 and abs(row_d - row_o) == 2)
 
@@ -453,7 +460,7 @@ class chess:
 
     def ischeck(self, player):
         king_position = None
-        king_symbol = "♔" if player == "white" else "♚"
+        king_symbol = self.whitepieces[5] if player == "white" else self.blackpieces[5]
 
         for i in range(64):
             if self.getpiece(i) == king_symbol:
